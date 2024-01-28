ab76a516e16c27b1c2fb6fb282e7e27793ba5f8c
LukaszNawara <152013230+LukaszNawara@users.noreply.github.com>
2024-01-28 09:33:26 +0100

    Add files via upload

---
+++ kurs_node_js/functions.js
@@ -0,0 +1,11 @@
+const sayHello = () => { console.log("Hello World") }
+const add = (a, b) => {
+console.log(a + b)
+}
+const someTitle = "example example"
+module.exports = {
+sayHello,
+add,
+someTitle
+}
+console.log("Everything is loaded correctly")
